6b7c9bb28ef36a3395eafad1cea195b0bb568481
Johnny Klironomos <ioannis.klironomos@gmail.com>
2018-01-05 13:35:19 +0200

    Introduced parser for each line to return command and arguments.
    - Raises error for empty lines.
    - Validates arguments for each command, based on specific patterns (regex)
    - Returns [cmd, args]

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,16 +1,44 @@
 class BitmapEditor
 
-  def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+  CMD_ARGS_REGEXES = {
+    "I" => /^[1-9][0-9]*\s[1-9][0-9]*$/,
+    "L" => /^[1-9][0-9]*\s[1-9][0-9]*\s[A-Z]$/,
+    "V" => /^[1-9][0-9]*\s[1-9][0-9]*\s[1-9][0-9]*\s[A-Z]$/,
+    "H" => /^[1-9][0-9]*\s[1-9][0-9]*\s[1-9][0-9]*\s[A-Z]$/
+  }.freeze
 
-    File.open(file).each do |line|
-      line = line.chomp
-      case line
-      when 'S'
-          puts "There is no image"
-      else
-          puts 'unrecognised command :('
+  def run(file)
+    raise StandardError.new("Please provide correct file") if file.nil? || !File.exists?(file)
+
+    File.open(file).each_with_index do |line, i|
+      cmd, args = parse_line(line, i + 1)
+
+      case cmd
+        else
+          raise InvalidCommandError.new("Unrecognised command `#{cmd}` - accepts I, S, C, L, V, H")
       end
     end
   end
+
+  private
+
+  def parse_line(line, line_num)
+    line.chomp!
+    raise InvalidCommandError.new("Empty line in file - line #{line_num} :(") if line.empty?
+
+    cmd, args = line.split(/\s/, 2)
+    validate_cmd_args(cmd, args, line_num)
+
+    args = args.split(/\s/) unless args.nil?
+    [cmd, args]
+  end
+
+  def validate_cmd_args(cmd, args, line_num)
+    regex = CMD_ARGS_REGEXES[cmd]
+    raise InvalidArgumentError.new("Invalid arguments for `#{cmd}` command - line #{line_num}") if regex && args !~ regex
+    true
+  end
 end
+
+class InvalidCommandError < StandardError; end
+class InvalidArgumentError < StandardError; end
