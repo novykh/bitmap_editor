3d53fe2e97ed84d54fcef2ba4fb8c5f526a70c1b
Johnny Klironomos <ioannis.klironomos@gmail.com>
2018-01-05 13:51:20 +0200

    Added `L`, `V` and `H` commands for drawing the image.
    - The logic for drawing is the same, even if there is a range or a single column/row, that way private method `update_matrix` is being reused by all the draw methods.

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -23,6 +23,12 @@ class BitmapEditor
           build_matrix(*args)
         when "S"
           return print_result
+        when "L"
+          draw_pixel(*args)
+        when "V"
+          draw_vertical_segment(*args)
+        when "H"
+          draw_horizontal_segment(*args)
         else
           raise InvalidCommandError.new("Unrecognised command `#{cmd}` - accepts I, S, C, L, V, H")
       end
@@ -56,11 +62,62 @@ class BitmapEditor
     @matrix = Matrix.build(rows, columns) {|m| WHITE}
   end
 
+
+  def draw_pixel(column, row, color)
+    update_matrix(color, {
+      start_row: coerce_to_cell_idx(row),
+      start_column: coerce_to_cell_idx(column)
+    })
+  end
+
+  def draw_vertical_segment(column, start_row, end_row, color)
+    update_matrix(color, {
+      start_column: coerce_to_cell_idx(column),
+      start_row: coerce_to_cell_idx(start_row),
+      end_row: coerce_to_cell_idx(end_row)
+    })
+  end
+
+  def draw_horizontal_segment(start_column, end_column, row, color)
+    update_matrix(color, {
+      start_row: coerce_to_cell_idx(row),
+      start_column: coerce_to_cell_idx(start_column),
+      end_column: coerce_to_cell_idx(end_column)
+    })
+  end
+
+  def update_matrix(color, start_row:nil, end_row:nil, start_column:nil, end_column:nil)
+    arr = matrix.to_a
+    end_row ||= start_row
+    end_column ||= start_column
+
+    raise InvalidArgumentError.new("Start row out of bounds") if start_row > rows
+    raise InvalidArgumentError.new("End row smaller than start row") if end_row < start_row
+    raise InvalidArgumentError.new("End row out of bounds") if end_row > rows
+
+    raise InvalidArgumentError.new("Start column out of bounds") if start_column > columns
+    raise InvalidArgumentError.new("End column smaller than start column") if end_column < start_column
+    raise InvalidArgumentError.new("End column out of bounds") if end_column > columns
+
+
+    for r in start_row..end_row do
+      for c in start_column..end_column do
+        arr[r][c] = color;
+      end
+    end
+
+    @matrix = Matrix[*arr]
+  end
+
   def print_result
     puts "There is no image" and return if matrix.nil?
 
     matrix_string = matrix.to_a.map{|r| r.join("")}.join("\n")
     puts matrix_string
+  end
+
+  def coerce_to_cell_idx(num)
+    num.to_i - 1
   end
 end
 
